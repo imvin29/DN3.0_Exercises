1025cf21596cbfbc2f37eaa8091c126468f4b623
Vineet Patnaik <137261994+imvin29@users.noreply.github.com>
2024-08-28 15:14:24 +0530

    Delete 15. Online_Bookstore-API Documentation with Swagger directory

--- 15. Online_Bookstore-API Documentation with Swagger/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-
-
-import com.example.BookstoreAPI.models.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRepository extends JpaRepository<Book, Integer> {
-}
--- 15. Online_Bookstore-API Documentation with Swagger/BookstoreAPI/src/main/java/com/example/BookstoreAPI/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.BookstoreAPI.repository;
-
-import com.example.BookstoreAPI.models.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
